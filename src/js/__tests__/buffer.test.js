d4ded89558fcb3d15ddf8f9a9817b2d8c7bf7166
GusevADresume <resumeGusevAD@gmail.com>
2022-10-09 00:34:39 +0300

    v1

---
+++ src/js/__tests__/buffer.test.js
@@ -0,0 +1,12 @@
+import  ArrayBufferConverter from '../buffer.js';
+import getBuffer from '../buffer.js';
+
+test('buferToString', () => {
+  const bufConverter = new ArrayBufferConverter();
+  const buf = new getBuffer();
+  bufConverter.load(buf);
+  const result = bufConverter.toString();
+  const toBe = {"data":{"user":{"id":1,"name":"Hitman","level":10}}}
+
+  expect(result).toBe(toBe);
+});
---
+++ src/js/buffer.js
@@ -0,0 +1,33 @@
+export default class ArrayBufferConverter {
+  constructor() {
+    this.buffer = undefined;
+  }
+
+  load(buffer) {
+    this.buffer = new Uint8Array(buffer);
+  }
+
+  toString() {
+    let str = '';
+    for (let i = 0; i < this.buffer.length; i++) {
+      str += String.fromCharCode(this.buffer[i]);
+    }
+    return (str);
+  }
+}
+
+export function getBuffer() {
+  const data = '{"data":{"user":{"id":1,"name":"Hitman","level":10}}}';
+  return ((input) => {
+    const buffer = new ArrayBuffer(data.length * 2);
+    const bufferView = new Uint16Array(buffer);
+    for (let i = 0; i < input.length; i++) {
+      bufferView[i] = input.charCodeAt(i);
+    }
+    return buffer;
+  })(data);
+}
+
+// let bf = new ArrayBufferConverter();
+// bf.load(getBuffer());
+// bf.toString();
